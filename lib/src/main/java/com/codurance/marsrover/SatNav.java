a5a1e7813602e0d15792ab07cb02d661aaed6ae2
George Harris <citizenharris@gmail.com>
2021-03-10 14:30:48 +0000

    Add tests for starting position W

--- lib/src/main/java/com/codurance/marsrover/SatNav.java
+++ lib/src/main/java/com/codurance/marsrover/SatNav.java
@@ -4,6 +4,7 @@ public class SatNav implements Navigation {
     public char execute(char command, char curDirection, int[] curLocation){
         if(curDirection == 'N' && command == 'L') return 'W';
         if(curDirection == 'N' && command == 'R') return 'E';
-        return 'S';
+        if(curDirection == 'W' && command == 'L') return 'S';
+        return 'N';
     }
 }
--- lib/src/test/java/com/codurance/marsrover/NavigationShould.java
+++ lib/src/test/java/com/codurance/marsrover/NavigationShould.java
@@ -1,19 +1,21 @@
 package com.codurance.marsrover;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NavigationShould {
 
-  @Test
-  public void test() {
-    assertEquals(true, true);
+  private Navigation navigation;
+
+  @BeforeEach
+  public void setUp() {
+    navigation = new SatNav();
   }
 
   @Test
   public void face_west_after_receiving_left(){
-    Navigation navigation = new SatNav();
     char newDirection = navigation.execute('L', 'N', new int[]{5,5});
     assertEquals('W', newDirection);
 
@@ -21,16 +23,20 @@ public class NavigationShould {
 
   @Test
   public void face_east_after_receiving_right(){
-    Navigation navigation = new SatNav();
     char newDirection = navigation.execute('R', 'N', new int[]{5,5});
     assertEquals('E', newDirection);
   }
 
   @Test
   public void face_south_after_receiving_left_from_position_west(){
-    Navigation navigation = new SatNav();
     char newDirection = navigation.execute('L', 'W', new int[]{5,5});
     assertEquals('S', newDirection);
+  }
+
+  @Test
+  public void face_north_after_receiving_right_from_position_west(){
+    char newDirection = navigation.execute('R', 'W', new int[]{5,5});
+    assertEquals('N', newDirection);
   }
 
 }
