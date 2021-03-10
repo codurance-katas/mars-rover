05348c6834e052899ee0857a73796d054b9a1bc7
sb48 <sophiebiber@icloud.com>
2021-03-10 13:53:29 +0000

    Creating first test for propulsion

--- lib/src/test/java/com/codurance/marsrover/PropulsionShould.java
+++ lib/src/test/java/com/codurance/marsrover/PropulsionShould.java
@@ -4,8 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PropulsionShould {
 
-  public void test() {
+  public void move_north_once() {
+    TestPropulsion testProp = new TestPropulsion();
+    
+
     assertEquals(true, true);
   }
 
+}
+
+
+class TestPropulsion implements Propulsion {
+
+
+  @Override
+  public int[] execute(char command, char curDirection, int[] curLocation) {
+    int[] returnTest = new int[]{5,5};
+    return returnTest;
+  }
 }
